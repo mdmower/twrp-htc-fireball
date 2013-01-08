234ae1522caf7623a2f61cef8dfa7a4964d722c7
Matt Mower <mowerm@gmail.com>
2013-01-08 00:22:22 -0600

    TWRP Mount USB Support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,8 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8
 BOARD_FORCE_RAMDISK_ADDRESS := 0x81800000
 
 TARGET_PREBUILT_KERNEL := device/htc/fireball/kernel
+# TARGET_KERNEL_SOURCE := kernel/htc/msm8960
+# TARGET_KERNEL_CONFIG := fireball_defconfig
 
 # Lights
 TARGET_PROVIDES_LIBLIGHTS := true
@@ -24,23 +26,25 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x47fffc00
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x49fffe00
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Recovery: set depending on recovery being built for. (CWM or TWRP)
-#           both init scripts can be found in the recovery folder
-TARGET_RECOVERY_INITRC := device/htc/fireball/recovery/init-cwm.rc
+# Recovery: set either cwm or twrp
+TARGET_RECOVERY_INITRC := device/htc/fireball/recovery/init-twrp.rc
 
 # Use power button as select in recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 
-# Use this flag if the board has a ext4 partition larger than 2gb
+# Board has an ext4 partition larger than 2gb
 BOARD_HAS_LARGE_FILESYSTEM := true
 
-#TWRP
+# TWRP
 DEVICE_RESOLUTION := 540x960
-TW_NO_USB_STORAGE := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_FLASH_FROM_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_INCLUDE_DUMLOCK := true
+
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
--- device_fireball.mk
+++ device_fireball.mk
@@ -3,6 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
+# common msm8960 configs
+$(call inherit-product, device/htc/msm8960-common/msm8960.mk)
+
 $(call inherit-product-if-exists, vendor/htc/fireball/fireball-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/htc/fireball/overlay
