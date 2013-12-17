6d81fd32568dfcb2fae3cebaab8c00324d9c6698
Matt Mower <mowerm@gmail.com>
2013-12-17 09:59:23 -0600

    Specify backlight brightness path
    * Allows TWRP to set and store a backlight brightness via Settings
    * Allows TWRP to blank and restore display via brightness values
    
    Change-Id: I9e9dbca0fb5630ad038034c04152a52d38fbf493

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,11 +49,14 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/
 
 # Charge mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 
 # TWRP
 DEVICE_RESOLUTION := 540x960
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_SCREEN_BLANK := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_FLASH_FROM_STORAGE := true
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
@@ -62,4 +65,3 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_INCLUDE_DUMLOCK := true
 TW_INCLUDE_JB_CRYPTO := true
-TW_NO_SCREEN_TIMEOUT := true
