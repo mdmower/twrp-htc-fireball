6b8d0438f4a6cdd5aabc0e2bb33f3c1ec5e82002
Matt Mower <mowerm@gmail.com>
2014-04-01 18:07:55 -0500

    Override product name
    Change-Id: I3b70effc241a200167de96f076560ab3d6a34e8a

--- cm.mk
+++ cm.mk
@@ -5,11 +5,14 @@ PRODUCT_RELEASE_NAME := fireball
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/fireball/device_fireball.mk)
+$(call inherit-product, device/htc/fireball/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fireball
 PRODUCT_NAME := cm_fireball
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := fireball
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := Incredible 4G LTE
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=fireball
